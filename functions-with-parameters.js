5fc4b3d8e22f9c7556e2f2ab4c4fdc35b97af561
Developsom <zana@cleancflo.nl>
2022-04-03 13:31:42 +0200

    functions and functions-with-parameters

--- functions-with-parameters.js
+++ functions-with-parameters.js
@@ -16,8 +16,10 @@ console.log(result);
 // ---- Verwachte uitkomsten:
 // greeter("Jan") geeft "Hoi Jan!"
 // greeter("Kees") geeft "Hoi Kees!"
-
-
+function greeter(name) {
+  console.log('Hoi ' + name + '!');
+}
+greeter('Zana')
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd minutesToSeconds, die een hoeveelheid minuten verwacht (als een getal) en teruggeeft hoeveel seconden dat zijn.
@@ -26,7 +28,12 @@ console.log(result);
 // minutesToSeconds(3) geeft 180
 // minutesToSeconds(23) geeft 1380
 
-
+function minutesToSeconds (minutes){
+  return console.log(minutes * 60)
+}
+minutesToSeconds(1)
+minutesToSeconds(3)
+minutesToSeconds(23)
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd merge, die twee strings verwacht en deze aan elkaar geplakt teruggeeft.
@@ -35,7 +42,14 @@ console.log(result);
 // merge("abra", "cadabra") geeft "abracadabra"
 // merge("zoet", "sappig") geeft "zoetsappig"
 
-
+function merge(string1, string2) {
+  //*Function* *FunctionName* *(FunctionValues, FunctionValues*
+  return console.log(string1 + string2)
+  //*Return*= Gives assignment on what to call *Console.log(Merge function values by '+')
+}
+merge('abra','cadabra')
+//*CallFunctionName* -> (Call function value by opening with *''* and call second by closing with *,*)
+merge('zoet','sappig')
 
 
 /* Opdracht  4 */
@@ -44,7 +58,15 @@ console.log(result);
 // calculateDogYears(6) geeft "Jouw hond is 42 jaar oud in mensenjaren."
 // calculateDogYears(2) geeft "Jouw hond is 14 jaar oud in mensenjaren."
 
-
+function calculateDogYears(ageOfDog) {
+  // *function* *functionname* *functionValueExpectation*
+  const multiplier = ageOfDog * 7
+  //*Added const to add the multiplier* *Const name to call upon later on* *Call upon functionvalue to multiply by destined value*
+  return  console.log('Jouw hond is ' + multiplier + ' jaar oud in mensenjaren')
+  // Return consolelog with sentence and call upon const where needed
+}
+calculateDogYears(6)
+calculateDogYears(2)
 
 /* Opdracht 5 */
 // Schrijf een functie genaamd wrapper, die twee parameters verwacht: een woord en een karakter
@@ -53,13 +75,25 @@ console.log(result);
 // wrapper("bril", "*") geeft "*bril*"
 // wrapper("beep", "_") geeft "_beep_"
 // wrapper("kaas", "Q") geeft "QkaasQ"
-
-
+ function wrapper(word, character) {
+   // *function* *functionname* *expected function values*
+   const mechanism = character + word + character
+   // implemented what i want my function to do inside a CONST variable
+   return console.log(mechanism)
+   // console log with simply the mechanism inside
+ }
+wrapper('bril', '*')
+wrapper('beep', '"')
+wrapper('kaas','Q')
+//*functionname*: to call upon my function -> added the values by opening with ' and distance them by ,
 
 /* Bonus opdracht  */
 // Schrijf een functie genaamd createDetailString, die een object met de properties firstName, lastName en profession verwacht en een zin teruggeeft
 // ---- Verwachte uitkomsten:
 // createDetailString({ firstName: 'Jan', lastName: 'Jansen', profession: 'docent'}) geeft "Het beroep vam Jan Jansen is docent."
 // createDetailString({ firstName: 'Kees', lastName: 'Klaasen', profession: 'brandweerman'}) geeft "Het beroep vam Kees Klaasen is brandweerman."
-
-
+function createDetailString(firstName, lastName, profession){
+return console.log('Het beroep van ' + firstName + ' ' + lastName + ' is ' + profession + '.')
+}
+createDetailString('Jan', 'Jansen', 'docent')
+createDetailString('Kees', 'Klaasen', 'brandweerman')
--- functions.js
+++ functions.js
@@ -6,28 +6,46 @@ function morningGreeter() {
   return "Goedemorgen";
 }
 
-const greeting = morningGreeter();
+//const greeting = morningGreeter();
 
-console.log(greeting);
+console.log(morningGreeter());
 
 /* Opdracht 1 */
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
-
+function getSchool() {
+  return "Novi Hogeschool"
+}
+console.log(getSchool())
 
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
 
-
-
+function complimentBot() {
+  return "Lekker bezig met die functies, Zana!"
+}
+console.log(complimentBot())
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
 
-
-
+function getGrades() {
+  const digits = [8, 8.5, 6, 7]
+  return  digits
+}
+console.log(getGrades())
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+
+function getDetails() {
+  const details = {
+    firstName: 'Zana',
+    lastName: 'Saeed'
+  }
+return details
+}
+
+console.log(getDetails())
